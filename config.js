fbf65df3237eee70a0636ab9e826066bc81c4a13
Ganesh Iyer <lastlegion@gmail.com>
2016-05-17 02:12:42 +0000

    new bindaas routes

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 
 var dataLoader_py = process.dataLoader_py || "/root/dataloader/DataLoader/dataLoader/dataloader.py";
 var api_key = process.dataloader_api_key || "4fbb38a3-1821-436c-a44d-8d3bc5efd33e";
-var api_submit_entry = process.dataloader_api_submit_entry ||  "http://localhost:9099/services/TCGA/GeoJSONMetaData/submit/json";
+var host = process.dataloader_host;
+var api_submit_entry = process.dataloader_api_submit_entry ||  host+"/services/Camicroscope_DataLoader/DataLoader/submit/json";
 var image_directory = process.dataLoader_image_directory ||  "/data/images";
-var api_get_md5_for_image = process.dataloader_api_subject_id_exists || "http://localhost:9099/services/TCGA/GeoJSONMetaData/query/getMD5forImage";
-var api_subject_id_exists = process.dataloader_api_subject_id_exists || "http://localhost:9099/services/TCGA/GeoJSONMetaData/query/getImagesForSubjectId";
+var api_get_md5_for_image = process.dataloader_api_subject_id_exists || host+"/services/Camicroscope_DataLoader/DataLoader/query/getMD5forImage";
+var api_subject_id_exists = process.dataloader_api_subject_id_exists || host+"/services/Camicroscope_DataLoader/DataLoader/query/getImagesForSubjectId";
 var port = process.dataloader_port || 3002;
 
 exports.dataLoader_py = dataLoader_py;
